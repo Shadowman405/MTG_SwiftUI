4613f6291a80b124ef1e579d1a14d10c4ff93a6a
m_mitin <maximus405@gmail.com>
2022-12-25 21:15:55 +0300

    need to rework download Image logic

---
+++ MTG_SwiftUI/Views/CardCell.swift
@@ -0,0 +1,22 @@
+//
+//  CardCell.swift
+//  MTG_SwiftUI
+//
+//  Created by Maxim Mitin on 25.12.22.
+//
+
+import SwiftUI
+
+struct CardCell: View {
+    //var card: CardMTG
+    
+    var body: some View {
+        Text("Card")
+    }
+}
+
+struct CardCell_Previews: PreviewProvider {
+    static var previews: some View {
+        CardCell()
+    }
+}
