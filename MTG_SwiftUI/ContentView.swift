e918327c154eb84099879e6986b91a27121ff884
m_mitin <maximus405@gmail.com>
2022-12-25 18:49:33 +0300

    Initial Commit

---
+++ MTG_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MTG_SwiftUI
+//
+//  Created by Maxim Mitin on 25.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MTG_SwiftUI/MTG_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_SwiftUIApp.swift
+//  MTG_SwiftUI
+//
+//  Created by Maxim Mitin on 25.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MTG_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
