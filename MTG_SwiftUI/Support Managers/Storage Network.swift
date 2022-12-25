d55a0b030030db54b2c20afb773f3b66506db94b
m_mitin <maximus405@gmail.com>
2022-12-25 19:08:43 +0300

    migrate Managers - Storage and Network

---
+++ MTG_SwiftUI/Support Managers/Storage Network.swift
@@ -0,0 +1,56 @@
+//
+//  Storage Network.swift
+//  MTG_SwiftUI
+//
+//  Created by Maxim Mitin on 25.12.22.
+//
+
+import Foundation
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    //MARK: - Save/delete collection
+    func save(cardCollection: [CardCollection]) {
+        write {
+            realm.add(cardCollection)
+        }
+    }
+    
+    func save(cardCollection: CardCollection) {
+        write {
+            realm.add(cardCollection)
+        }
+    }
+    
+    func delete(cardCollection: CardCollection) {
+        write {
+            realm.delete(cardCollection.cards)
+            realm.delete(cardCollection)
+        }
+    }
+    
+    //MARK: - Save CArd
+    
+    func save(card: CardMTG,in cardCollection: CardCollection){
+        write {
+            cardCollection.cards.append(card)
+        }
+    }
+    
+    
+    private func write(_ completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
