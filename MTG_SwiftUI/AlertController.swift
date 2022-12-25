743cdeb0ab11d75554a724ea1da07f848a60e55c
m_mitin <maximus405@gmail.com>
2022-12-25 19:10:44 +0300

    add reworked Alert Controller

---
+++ MTG_SwiftUI/AlertController.swift
@@ -0,0 +1,31 @@
+//
+//  AlertController.swift
+//  MTG_SwiftUI
+//
+//  Created by Maxim Mitin on 25.12.22.
+//
+
+import UIKit
+
+extension UIAlertController {
+    static func createAlert(withTitle title: String, andMessage message:String) -> UIAlertController {
+        UIAlertController(title: title, message: message, preferredStyle: .alert)
+    }
+    
+    func action(completion: @escaping (String) -> Void) {
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let newValue = self.textFields?.first?.text else {return}
+            guard !newValue.isEmpty else {return}
+            completion(newValue)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        addAction(saveAction)
+        addAction(cancelAction)
+        addTextField{ txtField in
+            txtField.placeholder = "Collection Name"
+        }
+    }
+    
+}
