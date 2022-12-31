677a31466603dfe49b71ee28132313418cfc067b
m_mitin <maximus405@gmail.com>
2022-12-31 04:34:32 +0300

    looks like problem with Realm resolved for collections

--- MTG_SwiftUI/Views/CollectionsView.swift
+++ MTG_SwiftUI/Views/CollectionsView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 import RealmSwift
 
 struct CollectionsView: View {
-    @EnvironmentObject var dataManager: StorageManager
+    //@EnvironmentObject var dataManager: StorageManager
     @ObservedResults(CardCollectionUI.self) var collections
     
     var body: some View {
         NavigationView {
             List(collections, id: \.id){ collection in
                 Text(collection.collectionName)
+            }
+        }
+        .listStyle(GroupedListStyle())
+        .navigationBarTitle("Collections", displayMode: .large)
+        .navigationBarBackButtonHidden(false)
+        .navigationBarItems(trailing: EditButton())
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    $collections.append(CardCollectionUI())
+                } label: {
+                    Image(systemName: "plus")
+                }
+
             }
         }
     }
@@ -25,6 +39,5 @@ struct CollectionsView_Previews: PreviewProvider {
     static var previews: some View {
         
         CollectionsView()
-            .environmentObject(StorageManager())
     }
 }
