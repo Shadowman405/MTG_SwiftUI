3ad186c8b5787b0acf347c4072ec97c70b1e506a
m_mitin <maximus405@gmail.com>
2023-01-08 00:29:45 +0300

    more configuration to subview
    add subview to main view

--- MTG_SwiftUI/Views/HealthCounterView.swift
+++ MTG_SwiftUI/Views/HealthCounterView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HealthCounterView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HpMiniView(color: .red)
+            
+            HpMiniView(color: .blue)
+        }
     }
 }
 
--- MTG_SwiftUI/Views/HpMiniView.swift
+++ MTG_SwiftUI/Views/HpMiniView.swift
@@ -10,18 +10,27 @@ import SwiftUI
 struct HpMiniView: View {
     @State private var hp = 0
     @State private var showAlert = false
+    @State var color = UIColor.red
  
     var body: some View {
         ZStack {
-            Color(.red)
+            Color(color)
             HStack {
                 Button {
                     self.hp += 1
                 } label: {
                     Text("+")
                 }
+                .offset(x: -90)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .font(.system(size: 40))
                 
                 Text("\(hp)")
+                    .fontWeight(.bold)
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                
                 
                 Button {
                     if hp != 0 {
@@ -33,6 +42,10 @@ struct HpMiniView: View {
                 } label: {
                     Text("-")
                 }
+                .offset(x: 90)
+                .foregroundColor(.white)
+                .fontWeight(.bold)
+                .font(.system(size: 40))
             }
         }
         .alert(isPresented: $showAlert) {
